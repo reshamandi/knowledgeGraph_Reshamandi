62c16cc7631e5b14ba559cd6acf8a64916042362
Harsha H L <harshahl@Harshas-MacBook-Air.local>
2022-05-09 01:11:30 +0530

    Update transaction fetching query

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -27,16 +27,25 @@ def findStock(ty="",cat="",bor="",col=""):
             if str(i).find(ty) != -1 and str(i).find(cat) != -1 and str(i).find(bor) != -1 and str(i).find(col) != -1:
                 data[0] += g.nodes[i]['Stock']
                 data[1].append(g[i])
-        return data
+    return data
 
 def index(request):
+  formData = {
+          'types': ["Accessories", "Art-silk", "Bagru", "Banarasi", "Bhagalpuri", "Chanderi-cotton", "Cotton-linen", "Cotton-tant", "Cotton-voile"],
+          'cat': "ee"
+      }
 
   if request.method == "POST":
-    data = findStock(request.POST.get("type"),request.POST.get("category"),request.POST.get("border"),request.POST.get("color"))
-    # return redirect('/members')
-    return render(request, 'first.html', {'stock': data[0], 'table': data[1] }) 
+      types = request.POST.getlist('type')
+    #   print(type(request.POST.getlist('type')))
+      print(types[1])
+      data = findStock(request.POST.get("type"),request.POST.get("category"),request.POST.get("border"),request.POST.get("color"))
+      # return redirect('/members')
+      
+      return render(request, 'first.html', {'stock': data[0], 'table': data[1] , 'formData': formData}) 
+
     
 
 
   else:
-    return render(request, 'first.html', {'stock': 0, 'table': "" }) 
+      return render(request, 'first.html', {'stock': 0, 'table': "" , 'formData': formData}) 
