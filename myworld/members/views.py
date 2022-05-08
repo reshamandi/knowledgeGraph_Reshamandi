d3210208d76052689bf6fb3bc7a4ffdeb3244262
Harsha H L <harshahl@Harshas-MacBook-Air.local>
2022-05-09 03:26:45 +0530

    Add template for table

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -5,12 +5,38 @@ import os, pickle
 import csv
 import networkx as nx
 
-def findStock(ty="",cat="",bor="",col=""):
-    data = [0, []]
+def load():
+    global g
     g = nx.MultiGraph()
     path = os.path.dirname(os.path.realpath(__file__)) + "/Graph1.txt"
     g = pickle.load(open(path,'rb'))
 
+def transactions(months, centres, ratings, wids):
+    data = [0, []]
+    if wids:
+        print("Yess")
+        for wid in wids:
+            for pdt in g[wid].keys():
+                for index in g[wid][pdt].keys():
+                    transaction = g[wid][pdt][index]
+                    if transaction['month'] in months and transaction['centre'] in centres and transaction['rating'] in ratings:
+                        data[1].append(transaction)
+                        data[0] += 1
+
+    else:
+        print("NO")
+        for mon in months:
+            print(mon)
+            for pdt in g[mon].keys():
+                for index in g[mon][pdt].keys():
+                    transaction = g[mon][pdt][index]
+                    if transaction['centre'] in centres and transaction['rating'] in ratings:
+                        data[1].append(transaction)
+                        data[0] += 1
+    return data
+
+def findStock(ty="",cat="",bor="",col=""):
+    data = [0, []]
     if ty=="" and cat=="" and bor=="" and col=="":
         print("Atleast one attribute/filter must be specified")
     else:        
@@ -32,20 +58,42 @@ def findStock(ty="",cat="",bor="",col=""):
 def index(request):
   formData = {
           'types': ["Accessories", "Art-silk", "Bagru", "Banarasi", "Bhagalpuri", "Chanderi-cotton", "Cotton-linen", "Cotton-tant", "Cotton-voile"],
-          'cat': "ee"
+          'categories': ["AC Blanket(DOHAR)", "Beads", "Bedsheet", "Bermuda", "Bluse", "Chiffon", "Crochet Lace","Cutting Roll", "Fabric", "Fusing", "Girls-Womens Suit", "Shorts"],
+          'borders': ["Checks", "Floral", "Golden-Zari", "Lines"],
+          'colors': ["Blue", "Red", "Orange", "Violet", "Green", "Yellow", "Indigo"],
+          'months': ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+          'centres': ["Bangalore", "Hyderabad", "Mumbai"],
+          'ratings': ["1s", "2s", "3s", "4s", "5s"]
       }
 
   if request.method == "POST":
-      types = request.POST.getlist('type')
-    #   print(type(request.POST.getlist('type')))
-      print(types[1])
-      data = findStock(request.POST.get("type"),request.POST.get("category"),request.POST.get("border"),request.POST.get("color"))
-      # return redirect('/members')
+
+        if request.POST.get('switch') == "product":
+            print("pdt")
+    
+        else: 
+            print("Trans")
+            months = request.POST.getlist('month')
+            centres = request.POST.getlist('centre')
+            ratings = request.POST.getlist('rating')
+            data = transactions(months, centres, ratings, [])
+            return render(request, 'first.html', {'count': data[0], 'table': data[1] , 'formData': formData}) 
+            print(months, centres, ratings)
+
+    #   types = request.POST.getlist('type')
+    #   category = request.POST.getlist('category')
+    # #   print(type(request.POST.getlist('type')))
+    #   print(types)
+    #   print(category)
+    #   print(request.POST.get('wids'))
+    #     data = findStock(request.POST.get("type"),request.POST.get("category"),request.POST.get("border"),request.POST.get("color"))
+        return redirect('/members')
       
-      return render(request, 'first.html', {'stock': data[0], 'table': data[1] , 'formData': formData}) 
+    #     return render(request, 'first.html', {'stock': data[0], 'table': data[1] , 'formData': formData}) 
 
     
 
 
   else:
-      return render(request, 'first.html', {'stock': 0, 'table': "" , 'formData': formData}) 
+        load()
+        return render(request, 'first.html', {'stock': 0, 'table': "" , 'formData': formData}) 
